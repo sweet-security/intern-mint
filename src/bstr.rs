82141f0dbc4d27353a960b01de685ccbf9f672c4
Ofek Shaltiel <ofeks@sweet.security>
2025-07-13 19:22:19 +0300

    no need to implement as_bstr helpers for interned now

--- src/bstr.rs
+++ src/bstr.rs
@@ -10,24 +10,6 @@ use bstr::{BStr, BString, ByteSlice};
 
 use crate::{borrow::BorrowedInterned, interned::Interned};
 
-impl Interned {
-    pub fn as_bstr(&self) -> &BStr {
-        BStr::new(self.deref())
-    }
-
-    pub fn as_path(&self) -> Cow<'_, Path> {
-        self.as_bstr().to_path_lossy()
-    }
-
-    pub fn as_os_str(&self) -> Cow<'_, OsStr> {
-        self.as_bstr().to_os_str_lossy()
-    }
-
-    pub fn as_str(&self) -> Cow<'_, str> {
-        self.as_bstr().to_str_lossy()
-    }
-}
-
 impl BorrowedInterned {
     pub fn as_bstr(&self) -> &BStr {
         BStr::new(self.deref())
